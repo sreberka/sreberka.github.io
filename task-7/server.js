8162c38ef795314aeb760306d8932f7727f528cc
sreberka <vitalia.serebro@gmail.com>
2018-01-26 19:24:05 +0300

    removed json file and implemented methods

--- task-7/server.js
+++ task-7/server.js
@@ -1,29 +1,46 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-const json = require('./object.json');
+let data = [
+    {
+        "blogId": "1",
+        "author": "Mary L"
+    },
+    {
+        "blogId": "2",
+        "author": "Kate D"
+    },
+    {
+        "blogId": "3",
+        "author": "Sam H"
+    }
+]
 
 app.all('/', function (req, res) {
-    res.json(json);
+    res.json(data);
 });
 
 app.route('/blogs')
     .get(function(req, res) {
-        res.json(json);
+        res.json(data);
     })
     .post(function(req, res) {
-        res.json(json);
+        res.json(data);
     });
 
-app.route('/blogs/:userId')
+app.route('/blogs/:blogId')
     .get(function(req, res) {
-        res.json(json);
+        res.json(data);
     })
     .put(function(req, res) {
-        res.send('Updated!!!');
+        let obj = {};
+        obj['blogId'] = req.params.blogId;
+        data.push(obj);
+        res.json(data);
     })
     .delete(function(req, res) {
-        res.send('Deleted!!!');
+        data = data.filter(obj => obj['blogId'] !== req.params.blogId);
+        res.send(data);
     });
 
 app.listen(port, () => {
