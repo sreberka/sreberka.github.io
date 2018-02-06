5ea2aae2fd32bab16809ec3bd557730791cf5415
sreberka <vitalia.serebro@gmail.com>
2018-02-06 13:38:54 +0300

    fixed structure

--- task-8/server.js
+++ task-8/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const router = require('./routes/index');
-const error = require('./error');
+const error = require('./middlewares/error');
 
 app.use('/', router);
 app.use(error);
