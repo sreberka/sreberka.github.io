3ae9d1d0341b13e81e3d300b6c594af35949130d
sreberka <vitalia.serebro@gmail.com>
2018-03-12 18:03:04 +0300

    for redux

--- task-10/src/app.js
+++ task-10/src/app.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import Blog from './components/Blog';
 
+import { combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from '../reducers';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <Blog />,
+  <Provider store={store}>
+    {() => <Blog /> }
+  </Provider>,
   document.getElementById('root')
 );
