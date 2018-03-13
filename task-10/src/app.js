656d47a415521a4e61bf660682662b62ae161bbf
Dmitry K <example@example.com>
2018-03-13 17:50:11 +0300

    deleted redux folders

--- task-10/src/app.js
+++ task-10/src/app.js
@@ -3,16 +3,7 @@ import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import Blog from './components/Blog';
 
-import { combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import * as reducers from '../reducers';
-
-const reducer = combineReducers(reducers);
-const store = createStore(reducer);
-
 ReactDOM.render(
-  <Provider store={store}>
-    {() => <Blog /> }
-  </Provider>,
+  <Blog />,
   document.getElementById('root')
 );
