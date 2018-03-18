151cffe188687687fab96a829b5635a1d9cfd9eb
sreberka <vitalia.serebro@gmail.com>
2018-03-18 19:45:58 +0300

    Added sorting and filtering

--- task-11/src/js/controllers/TodoCtrl.js
+++ task-11/src/js/controllers/TodoCtrl.js
@@ -1,9 +1,13 @@
 angular.module('todolist')
-    .controller('TodoCtrl', function ($scope) {
-        $scope.items = [];
+    .controller('TodoCtrl', function ($scope, $http) {
+        $http({method: 'GET', url: 'src/todos/todos.json'}).
+        then(function success(response) {
+            $scope.items=response.data.todos;
+        });
+
         let itemStatus = '';
         $scope.addTodo = function () {
-            let newItem = {name: $scope.todoName, complited: false, date: new Date(), status: 'new'};
+            let newItem = {name: $scope.todoName, complited: false, date: Date.now(), status: 'new'};
             $scope.items.push(newItem);
             $scope.todoName = '';
         };
@@ -16,6 +20,7 @@ angular.module('todolist')
                 item.status = 'new';
             }
         };
+        $scope.selectedPeriod = { id: 0, name: 'today' };
 
         $scope.statusFilter = function (item) {
             if(item.status.match(itemStatus)){
@@ -23,7 +28,31 @@ angular.module('todolist')
             }
         };
 
+        $scope.filterDate = function (item) {
+            if(item.date >= $scope.selectedPeriod.id){
+                return item
+            }
+        };
+
         $scope.changeFilter = function (query) {
             itemStatus = query;
         };
+
+        $scope.sortField = undefined;
+        $scope.reverse = false;
+        $scope.orderByQuery = function (query) {
+            if($scope.sortField === query){
+                $scope.reverse = !$scope.reverse;
+            } else {
+                $scope.sortField = query;
+                $scope.reverse = false;
+            }
+        };
+
+        $scope.options = [
+            { id: Date.now() - 5*86400000, name: '5 days' },
+            { id: Date.now() - 10*86400000, name: '10 days' },
+            { id: Date.now() - 15*86400000, name: '15 days' },
+            { id: Date.now() - 20*86400000, name: '20 days'}
+        ];
     });
