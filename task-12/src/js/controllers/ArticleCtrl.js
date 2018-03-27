1ab51a2c3ee3f07bedce72648e8c1465f6a4f8bf
sreberka <vitalia.serebro@gmail.com>
2018-03-27 21:24:54 +0300

    files fixes

--- task-12/src/js/controllers/ArticleCtrl.js
+++ task-12/src/js/controllers/ArticleCtrl.js
@@ -10,22 +10,21 @@ articleApp.controller('ArticleCtrl', function ($scope, $rootScope) {
 
         $scope.firstPage = function() {
             return $scope.currentPage == 0;
-        }
+        };
         $scope.lastPage = function() {
             var lastPageNum = Math.ceil($scope.items.length / $scope.itemsPerPage - 1);
             return $scope.currentPage == lastPageNum;
-        }
+        };
         $scope.numberOfPages = function(){
             return Math.ceil($scope.items.length / $scope.itemsPerPage);
-        }
+        };
         $scope.startingItem = function() {
             return $scope.currentPage * $scope.itemsPerPage;
-        }
+        };
         $scope.pageBack = function() {
             $scope.currentPage = $scope.currentPage - 1;
-        }
+        };
         $scope.pageForward = function() {
             $scope.currentPage = $scope.currentPage + 1;
         }
-
     });
