addd6e7f85a446e3ceee67a419541ad530b43932
sreberka <vitalia.serebro@gmail.com>
2018-04-16 12:02:44 +0300

    added 2 methods

--- react/task-1/src/app.js
+++ react/task-1/src/app.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import Hello from './components/Hello';
 
+// Component
 
 ReactDOM.render(
   <Hello />,
   document.getElementById('root')
 );
+
+
+// Create element
+
+// const child = React.createElement('p', null, 'Hello, World!!!');
+// const root = React.createElement('div', { className: 'hello-element' }, child);
+// ReactDOM.render(root, document.getElementById('root'));
