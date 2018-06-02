0d868b33185aa985d36246aa099c244c91045ae7
sreberka <vitalia.serebro@gmail.com>
2018-06-02 19:54:36 +0300

    mobile

--- automechta-new/js/js.js
+++ automechta-new/js/js.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function () {
+    $('#hamburger').click(function () {
+        $(this).toggleClass('open');
+        $('.mobile-menu').toggleClass('open-menu');
+        $('body').toggleClass('fixed-body');
+    });
+});
