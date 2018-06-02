9e04342d42bc9c645b991b9b7c4f0949f0361c26
sreberka <vitalia.serebro@gmail.com>
2018-06-02 21:53:50 +0300

    scroll fixes

--- automechta-new/js/js.js
+++ automechta-new/js/js.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $('#hamburger').click(function () {
         $(this).toggleClass('open');
         $('.mobile-menu').toggleClass('open-menu');
-        $('body').toggleClass('fixed-body');
+        $('html').toggleClass('fixed-body');
     });
 });
